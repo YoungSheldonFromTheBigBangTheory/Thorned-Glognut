7689d7e192840e322aa818154a5029e8d323fe13
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-03-23 13:54:08 +0000

    works

--- Glognut.py
+++ Glognut.py
@@ -1,6 +1,12 @@
 import math
 
-x = str("glognut")
-x = input()
+x = int(input("Enter Diameter, "))
+pi = math.pi
 
-print(x)
+r = x/2
+a = pi * (r ** 2)
+c = 2 * pi * r
+
+print("Radius = ", r)
+print("Area = ", a)
+print("Circumference = ", c)
